3e7ca88712007dcf431ebc16e2ed8878682958b7
Bruno Paschoal Mendes Bercot <brunobercot2012@gmail.com>
2018-11-27 19:26:37 -0200

    commit

--- Variaveis/Exemplo2.php
+++ Variaveis/Exemplo2.php
@@ -4,9 +4,15 @@ $anoNascimento = 1990;
 $nomeCompleto = "";
 //Na linha de baixo tem uma variavel com numero no nome
 $nome1 = "Joao";
+$nome2 = "Bruno";
 
+	echo $nome2;
+	echo "<br/>";
+	echo $nome1;
 
+unset($nome1);
 
+echo $nome2;
 
 
 ?>
