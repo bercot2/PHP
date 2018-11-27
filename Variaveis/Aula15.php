206a14abbf84bd821bbdac33af9fd614712fe9aa
Bruno Paschoal Mendes Bercot <brunobercot2012@gmail.com>
2018-11-27 20:34:16 -0200

    Comitando aula 15 Completa!

--- Variaveis/Aula15.php
+++ Variaveis/Aula15.php
@@ -4,7 +4,14 @@
 //(int) e um cast transformando a variavel $nome do tipo string para inteiro
 $nome = (int)$_GET["a"];
 
-var_dump($nome);
+//var_dump($nome);
+
+$ip = $_SERVER["REMOTE_ADDR"];
+$ip1 = $_SERVER["SCRIPT_NAME"];
+
+	echo $ip . " - " . "Variavel 1";
+	echo "<br/>";
+	echo $ip1 . " - " . "Variavel 2";
 
 
 ?>
