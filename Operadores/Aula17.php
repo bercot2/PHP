a290f11a1da80c1377ead1557bb5bb3171dbd6b4
Bruno Paschoal Mendes Bercot <brunobercot2012@gmail.com>
2018-11-28 00:18:27 -0200

    Adicionando arquivo Aula17

--- Operadores/Aula17.php
+++ Operadores/Aula17.php
@@ -30,6 +30,8 @@ echo "Valor Com desconto: ".$valortotal."<br/>";
 
 <?php
 
+//operadores de calculos
+
 $a = 10;
 $b = 2;
 
@@ -58,5 +60,42 @@ echo $a ** $b; //Operador de exponenciacao
 echo "<br/>";
 
 var_dump($a == $b); //operador de igualdade
+
+echo "<br/>";
+
+var_dump($a <=> $b); //operador de verificar de maior (1), igual (0), ou menor(-1)
+
+echo "<br/>";
+
+//funcao mostrando o operador nullcolesce, esse operador, ignora quando e nulo e mostra somente a primeira variavel encontrada com valor
+function op_null(){
+	$a = null;
+	$b = null;
+	$c = 10;
+	echo $a ?? $b ?? $c;
+} 
+
+echo op_null();
+
+?>
+
+
+
+<?php
+//Operadores incrementais
+
+$a = 10;
+
+echo "<br/>";
+
+echo $a ++; // Soma mais 1 no valor da variavel
+
+echo "<br/>";
+
+echo $a;
+
+echo "<br/>";
+
+echo -- $a; //diminui 1 no valor da variavel
 
 ?>
